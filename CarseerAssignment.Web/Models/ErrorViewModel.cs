8826c96e0199f63016022ae5aadb2a31d71e13e7
Ali <Ali@DESKTOP-RAVEM50>
2025-01-24 15:17:12 +0300

    Add project files.

---
+++ CarseerAssignment.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CarseerAssignment.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
